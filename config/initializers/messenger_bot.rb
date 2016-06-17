4a4e58c2c1a4ef41c31b358eaa612032e5f5a62f
arico93 <mizuki-ariga@dac.co.jp>
2016-06-17 06:35:41 +0000

    errors mended3

--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -1,5 +1,5 @@
 Messenger::Bot.config do |config|
-  config.access_token = EAADfkmpZAPzMBAAxyGRZC5ZB4PHfHqhzAVxd4dZC7pB3S71N1FjrgTqOmayNWQYpqwEF5dax7oNQXfbVJgs0iekDWeOaEfChm8hsAxuYc4RUZADdbeNU5ZACtjGtZBMdZCsjgobGrJhkotn8UFwaFPSZBcpnwTVOZBtUTCTdtW3A7JSQZDZD
-  config.validation_token = hoge
-  config.secret_token = d2d7ff748e48691f334070c96ce24a2d
+  config.access_token = "EAADfkmpZAPzMBAAxyGRZC5ZB4PHfHqhzAVxd4dZC7pB3S71N1FjrgTqOmayNWQYpqwEF5dax7oNQXfbVJgs0iekDWeOaEfChm8hsAxuYc4RUZADdbeNU5ZACtjGtZBMdZCsjgobGrJhkotn8UFwaFPSZBcpnwTVOZBtUTCTdtW3A7JSQZDZD"
+  config.validation_token = "hoge"
+  config.secret_token = "d2d7ff748e48691f334070c96ce24a2d"
 end
